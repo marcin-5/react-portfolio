9d454eed882ef6d115d0766bb287418409668340
Marcin Bojara <marcin_5@mail.ru>
2023-11-18 14:19:43 +0100

    handle switch language

--- src/features/Header.jsx
+++ src/features/Header.jsx
@@ -1,11 +1,15 @@
 import flagENImg from "@/assets/images/flag-en.png";
+import flagPLImg from "@/assets/images/flag-pl.png";
 import hashImg from "@/assets/images/hash.png";
 import logoImg from "@/assets/images/logo-d.png";
 import { Flex, HStack, Image, Link } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
 
 export function Header() {
-  const { t } = useTranslation("home");
+  const { t, i18n } = useTranslation("home");
+  const switchLanguage = () => {
+    i18n.changeLanguage(i18n.language === "en" ? "pl" : "en");
+  };
   return (
     <Flex justify={"space-between"} px={{ base: 0, md: 12, xl: 36 }}>
       <Image src={logoImg} h={14} />
@@ -19,7 +23,13 @@ export function Header() {
           <Image src={hashImg} h={8} mr={3} />
           {t("otw")}
         </Link>
-        <Image src={flagENImg} h={8} pl={{ base: 5, md: 10, xl: 20 }} cursor="pointer" />
+        <Image
+          onClick={switchLanguage}
+          src={i18n.language === "en" ? flagENImg : flagPLImg}
+          h={8}
+          pl={{ base: 5, md: 10, xl: 20 }}
+          cursor="pointer"
+        />
       </HStack>
     </Flex>
   );
