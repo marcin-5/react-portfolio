c5301523ba20d062c2dcc476fd7c6b4b23916850
Marcin Bojara <marcin_5@mail.ru>
2023-11-18 18:24:19 +0100

    add image slider component

---
+++ src/components/ImageSlider/ImageSlider.jsx
@@ -0,0 +1,62 @@
+import {
+  Box,
+  Image,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalOverlay,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
+
+export function ImageSlider({ imageList }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const openModalSlider = (imgIndex) => {
+    setCurrentImageIndex(imgIndex);
+    setIsModalOpen(true);
+  };
+  const renderImages = () => {
+    return imageList.map((imageLink, i) => {
+      return (
+        <Image
+          onClick={() => openModalSlider(i)}
+          src={imageLink}
+          boxShadow="xl"
+          borderRadius="xl"
+          key={imageLink}
+        />
+      );
+    });
+  };
+  const slider = (
+    <Slide
+      defaultIndex={currentImageIndex}
+      infinite={false}
+      arrows={imageList.length > 1}
+      autoplay={false}
+    >
+      {renderImages()}
+    </Slide>
+  );
+
+  const modalSlider = (
+    <Modal bg="transparent" size="6xl" isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+      <ModalOverlay />
+      <ModalContent bg="transparent" boxShadow={"none"}>
+        <ModalCloseButton zIndex={1} bg="primary.light" />
+        <ModalBody>{slider}</ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+
+  return (
+    <>
+      <Box w={350}>{slider}</Box>
+      {modalSlider}
+    </>
+  );
+}
