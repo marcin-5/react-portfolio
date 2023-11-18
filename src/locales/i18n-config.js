e6b16948952e3068cb954cebf29028f019b581bc
Marcin Bojara <marcin_5@mail.ru>
2023-11-18 16:16:01 +0100

    use default language from browser and cache

--- src/locales/i18n-config.js
+++ src/locales/i18n-config.js
@@ -2,15 +2,19 @@ import homeEN from "@/locales/en/home.json";
 import homePL from "@/locales/pl/home.json";
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
-i18next.use(initReactI18next).init({
-  resources: {
-    en: {
-      home: homeEN,
+i18next
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    resources: {
+      en: {
+        home: homeEN,
+      },
+      pl: {
+        home: homePL,
+      },
     },
-    pl: {
-      home: homePL,
-    },
-  },
-  fallbackLng: "en",
-});
+    fallbackLng: "en",
+  });
