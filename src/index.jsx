d8d36b2ed261fdd52a902354ff4b4ce1ab4fa114
Marcin Bojara <marcin_5@mail.ru>
2023-11-19 14:17:42 +0100

    add firebase setup

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,16 @@
-import "@fontsource/barlow";
+import "@/locales/i18n-config.js";
 import "@fontsource-variable/figtree";
 import "@fontsource-variable/rubik";
-import "@/locales/i18n-config.js";
+import "@fontsource/barlow";
 
 import { myTheme } from "@/styles/theme.js";
+import { FirebaseApp } from "@/utils/firebase.js";
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.jsx";
+
+FirebaseApp.init();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
